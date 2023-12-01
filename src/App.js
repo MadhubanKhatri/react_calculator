2d1d7ed0f87645aaa8e3ff6578a59aad57688d81
Madhuban Khatri <madhuban211998@gmail.com>
2023-12-01 12:36:40 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const arr = [0,1,2,3,4,5,6,7,8,9,"+","-","*","/","C"];
+  const [counter, setCounter] = useState("");
+
+  function handleClick(event){
+    const value = event.target.value;
+    setCounter(counter+value);
+
+  }
+
+  function handleEqual(){
+    setCounter(eval(counter));
+    console.log(eval(counter));
+  }
+
+  function clearScreen(){
+    setCounter("");
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container my-5 w-25 bg-warning py-5 rounded">
+      <input type="text" className="form-control fs-4" style={{textAlign:"right"}} value={counter} readOnly/>
+      <br/>
+      <div className="row">
+      {
+        arr.map((ele)=><div className="col-4 my-2">
+          <input type="button" className="btn btn-sm btn-primary w-100 fs-4" value={ele} onClick={(ele=="C"?clearScreen:handleClick)}/>
+        </div>)
+      }
+      </div>
+      <input type="button" className="btn btn-sm btn-primary w-100 fs-4" value={"="} onClick={handleEqual}/>
     </div>
   );
 }
